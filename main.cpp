7092c2b0ee580337316af5cba3e9efc482ba2f79
MacBook Pro <macbookpro@MacBook-Pro-som-tillhor-MacBook-2.local>
2025-09-24 21:37:13 +0200

    My first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int a = 5;
+    int b = 6;
+
+    int sum = a + b;
+
+    std::cout << "The sum of " << a << " and " << b << " is " << sum << std::endl;
+
+    return 0;
+}
